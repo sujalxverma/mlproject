704370836336f91d35f5d12caf0c5d63142dde07
sujal <sujalxv@gmail.com>
2026-03-29 22:49:49 +0530

    hyperparameter

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,4 +1,3 @@
-# TRAIN DIFFERENT MODELS AND COMPATE THEM.
 import os, sys
 from dataclasses import dataclass
 
@@ -16,7 +15,7 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_obj, evaluate_model
+from src.utils import save_obj, evaluate_models
 
 
 @dataclass
@@ -43,16 +42,48 @@ class ModelTrainer:
                 "AdaBoost": AdaBoostRegressor(),
             }
 
-            model_report = evaluate_model(
+            params = {
+                "DecisionTree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "RandomForest": {
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "GradientBoosting": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "LinearRegression": {},
+                "KNN": {},
+                "XGBRegressor": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoost": {
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost": {
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+
+            # 🔹 Evaluate models
+            model_report = evaluate_models(
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
                 models=models,
+                param=params
             )
 
-            best_model_score = max(model_report.values())
+            # 🔹 Get best model
             best_model_name = max(model_report, key=model_report.get)
+            best_model_score = model_report[best_model_name]
             best_model = models[best_model_name]
 
             if best_model_score < 0.6:
@@ -60,11 +91,16 @@ class ModelTrainer:
 
             logging.info(f"Best model: {best_model_name}")
 
+            # 🔹 IMPORTANT: Fit best model before saving (in case evaluate_models didn't)
+            best_model.fit(X_train, y_train)
+
+            # 🔹 Save model
             save_obj(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model,
             )
 
+            # 🔹 Evaluate on test set
             predicted = best_model.predict(X_test)
             r2_square = r2_score(y_test, predicted)
 
--- src/utils.py
+++ src/utils.py
@@ -1,40 +1,52 @@
 # COMMON FUNCTIONALITIES THAT EVERYONE CAN USE.
 
-import os,sys
+import os, sys
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.model_selection import GridSearchCV
 
-def save_obj(file_path , obj):
-    try :
+
+def save_obj(file_path, obj):
+    try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path , exist_ok=True)
-        with open(file_path , 'wb') as file_obj:
-            dill.dump(obj , file_obj)
-            
-    except Exception as e:
-        raise CustomException(e,sys)
-    
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
 
-def evaluate_model(X_train , y_train , X_test , y_test , models):
-    try :
-        
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
         report = {}
-        
+
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            
-            model.fit(X_train , y_train) # Train model
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            # model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
-            
-            train_model_score = r2_score(y_train , y_train_pred)
-            test_model_score = r2_score(y_test , y_test_pred)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
             report[list(models.keys())[i]] = test_model_score
-        
+
         return report
-    
+
     except Exception as e:
-        raise CustomException(e , sys)
+        raise CustomException(e, sys)
