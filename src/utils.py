4d699eda307da8d7a6cf22ce47fcc58a663d3955
sujal <sujalxv@gmail.com>
2026-04-06 23:31:31 +0530

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 # COMMON FUNCTIONALITIES THAT EVERYONE CAN USE.
 
 import os, sys
+import pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 import numpy as np
@@ -47,6 +48,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
